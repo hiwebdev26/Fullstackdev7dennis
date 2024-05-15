93127d420850c132d3187c5c5f3e0a197cddeeb0
hiwebdev26 <hiwebdev0026@gmail.com>
2024-05-14 22:39:01 -0700

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import { Hero } from "@/components/main/hero";
 import { Projects } from "@/components/main/projects";
 import { Skills } from "@/components/main/skills";
 
+
 export default function Home() {
+
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
--- constants/index.ts
+++ constants/index.ts
@@ -360,5 +360,5 @@ export const NAV_LINKS = [
 ] as const;
 
 export const LINKS = {
-  sourceCode: "https://github.com/sanidhyy/space-portfolio",
+  sourceCode: "https://github.com/hiwebdev26/FullStack_Dev-Portfolio",
 };
